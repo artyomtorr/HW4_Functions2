21c12af1695547bf7aa5fd7180cf5f3a02f7ea79
Artyom <toropov.01@bk.ru>
2023-09-27 19:31:09 +0300

    Change functions names and extend run_protein_tools

--- protein_tools.py
+++ protein_tools.py
@@ -1,4 +1,7 @@
 alphabet_protein = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'}
+
+alphabet_rna = {'A', 'U', 'G', 'C'}
+
 amino_acid_masses = {
     'A': 71.03711,
     'R': 156.10111,
@@ -21,7 +24,10 @@ amino_acid_masses = {
     'Y': 163.06333,
     'V': 99.06841
 }
-codon_table = {
+
+gydrophobic_aminoacids = {"A", "V", "L", "I", "P", "F", "W", "M"}
+
+dna_codons = {
         'A': ['GCT', 'GCC', 'GCA', 'GCG'],
         'C': ['TGT', 'TGC'],
         'D': ['GAT', 'GAC'],
@@ -41,29 +47,56 @@ codon_table = {
         'T': ['ACT', 'ACC', 'ACA', 'ACG'],
         'V': ['GTT', 'GTC', 'GTA', 'GTG'],
         'W': ['TGG'],
-        'Y': ['TAT', 'TAC']}
-gydrophobic_aminoacids = {"A", "V", "L", "I", "P", "F", "W", "M"}
+        'Y': ['TAT', 'TAC'],
+        '*': ["UAA", "UAG", "UGA"]}
+
+rna_codons = {
+        "F": ["UUC", "UUU"], "L": ["UUA", "UUG", "CUU", "CUC", "CUA", "CUG"],
+        "I": ["AUU", "AUC", "AUA"], "M": ["AUG"], "V": ["GUU", "GUC", "GUA", "GUG"],
+        "S": ["UCU", "UCC", "UCA", "UCG"], "P": ["CCU", "CCC", "CCA", "CCG"],
+        "T": ["ACU", "ACC", "ACA", "ACG"], "A": ["GCU", "GCC", "GCA", "GCG"],
+        "Y": ["UAC", "UAU"], "*": ["UAA", "UAG", "UGA"], "H": ["CAU", "CAC"],
+        "Q": ["CAA", "CAG"], "N": ["AAU", "AAC"],
+        "K": ["AAA", "AAG"], "D": ["GAU", "GAC"], "E": ["GAA", "GAG"],
+        "C": ["UGU", "UGC"], "W": ["UGG"], "R": ["CGU", "CGC", "CGA", "CGG", "AGA", "AGG"],
+        "S": ["AGU", "AGC"], "G": ["GGU", "GGC", "GGA", "GGG"]
+    }
 
 
-def compute_length(*seqs: str):
+def is_protein(seq):
+    unique_chars = set(seq.upper())
+    return unique_chars <= alphabet_protein
+
+
+def is_rna(seq):
+    unique_chars = set(seq.upper())
+    return unique_chars <= alphabet_rna
+
+
+def compute_molecular_weight(seq):
+    molecular_weight = 0
+    for amino_acid in seq:
+        molecular_weight += amino_acid_masses[amino_acid]
+    return round(molecular_weight, 3)
+
+
+def compute_length(seq: str):
     """
     Compute the length of the input amino acid sequence
     
     """
-    lens = []
-    for seq in seqs:
-        lens.append(len(seq))
-    return lens if len(lens) > 1 else lens[0]    
+    return len(seq)    
 
 
-def level_of_hydrophobic(protein):
+def compute_hydrophobicity(protein):
 
     count_of_gydrophobic = 0
-    for i in range(len(protein)):
-        if protein[i] in gydrophobic_aminoacids:
-            count_of_gydrophobic += 1
+    if is_protein(protein):
+        for i in range(len(protein)):
+            if protein[i] in gydrophobic_aminoacids:
+                count_of_gydrophobic += 1
 
-    percentage = count_of_gydrophobic / len(protein) * 100
+    percentage = round(count_of_gydrophobic / len(protein) * 100, 3)
 
     return f"Percentage of gydrophobic aminoacids in {protein} = {percentage}%."
 
@@ -74,23 +107,25 @@ def translation(seq):
     triplets = [seq[i:i + 3].upper() for i in range(0, len(seq), 3)]
     protein = []
     for triplet in triplets:
-        for aminoacid in codon_table.keys():
-            if triplet in codon_table[aminoacid]:
+        for aminoacid in rna_codons.keys():
+            if triplet in rna_codons[aminoacid]:
                 protein.append(aminoacid)
 
-    if is_protein("".join(protein)):
-        start = protein.index("M")
-        stop = protein.index("*")
-        return "".join(protein[start:stop + 1])
-    else:
-        return "This sequence doesn't include the gene."
+    start = protein.index("M")
+    stop = protein.index("*")
+    return "".join(protein[start:stop + 1])
 
 
-def mutations(seq, protein):
+def check_mutations(seq, protein):
+
+    if is_protein(protein[:-1]) is not True:
+        raise ValueError("Invalid protein sequence")
+    if is_rna(seq) is not True:
+        raise ValueError("Invalid RNA sequence")
+
     correct_protein = translation(seq)
-
-    
     bank_of_mutations = []
+    
     for i in range(len(correct_protein)):
         if correct_protein[i] != protein[i]:
             bank_of_mutations.append(f'{protein[i]}{i + 1}')
@@ -101,31 +136,25 @@ def mutations(seq, protein):
         return "Mutations:" + ", ".join(bank_of_mutations) + "."
 
 
-def is_protein(seq):
-    unique_chars = set(seq)
-    return unique_chars <= alphabet_protein
-
-
-def molecular_weight(seq):
-    molecular_weight = 0
-    for amino_acid in seq:
-        molecular_weight += amino_acid_masses[amino_acid]
-    return round(molecular_weight, 3)
-
-
 def run_protein_tools(*seqs_and_procedure):
     procedure = seqs_and_procedure[-1]
     seqs = seqs_and_procedure[:-1]
 
     results = []
-
-    for seq in seqs:
-        seq = seq.upper()
-        if is_protein(seq) is not True:
-            raise ValueError("Invalid alphabet")
-        if procedure == 'molecular_weight':
-            results.append(molecular_weight(seq))
-
+    if procedure == 'check_mutations':
+        results.append(check_mutations(seqs[0], seqs[1]))
+        
+    else:
+        for seq in seqs:
+            seq = seq.upper()
+            if is_protein(seq) is not True:
+                raise ValueError("Invalid protein sequence")
+            if procedure == 'compute_molecular_weight':
+                results.append(molecular_weight(seq))
+            elif procedure == 'compute_length':
+                results.append(compute_length(seq))
+            elif procedure == 'compute_hydrophobicity':
+                results.append(compute_hydrophobicity(seq))
     if len(results) == 1:
         return results[0]
     else:
